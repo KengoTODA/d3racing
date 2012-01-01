14fed2373d9ea19932af57dbfe99e20d82bd2f75
eller86 <skypencil@gmail.com>
2012-01-01 14:51:23 +0800

    optimize.sh removes unnecessary files, and creates tar.gz

--- optimize.sh
+++ optimize.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 rm -rf build
+rm builded.tar.gz
 r.js -o app.build.js
 
 # remove backup files
 rm -f build/*~
+
+# remove .git
+rm -rf build/.git
+
+# remove unnecessary javascript files
+for FILE in `ls build/*.js`
+do
+  if [ $FILE != 'build/game.js' ]; then
+    rm $FILE
+  fi
+done
+
+tar czvf builded.tar.gz build
